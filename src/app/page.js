a014df0c989af4ea1be1787bdb5a48cdbffd2904
kathir0206 <kathiresanmsc23@gmail.com>
2023-09-26 22:39:05 +0530

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
       </div>
 
       <div className={styles.grid}>
